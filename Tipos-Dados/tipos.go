c9a172d32b44850b742d41b4b81b14c3dd1df03f
DevVitorlevi <levivitor8@gmail.com>
2025-08-21 13:53:05 -0300

    feat: Tipos

---
+++ Tipos-Dados/tipos.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numInt1 int = 100
+	fmt.Println(numInt1)
+
+	numInt2 := 101
+	fmt.Println(numInt2)
+
+	var numFloat float32 = 111.222
+	fmt.Println(numFloat)
+
+	numFloat2 := 122.221
+	fmt.Println(numFloat2)
+
+	var nome string = "Vitor"
+	fmt.Println(nome)
+
+	nome2 := "Levi"
+	fmt.Println(nome2)
+
+	// Outra Forma
+
+	var nome12 int
+	fmt.Println(nome12)
+
+	var boolean bool
+	fmt.Println(boolean)
+
+	var erro error
+	fmt.Println(erro)
+}
