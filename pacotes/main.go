eb605bd9a76e03810e7f9d6dfffd07829cf87afb
DevVitorlevi <levivitor8@gmail.com>
2025-08-21 13:07:25 -0300

    feat:Pacotes externos

--- pacotes/main.go
+++ pacotes/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 	"modulo/auxi"
 )
 
 func main() {
 	fmt.Println("Escrevendo dentro do pacote main")
 	auxi.Escrever()
+	error := checkmail.ValidateFormat("Vitor123@gmail.com")
+	fmt.Println(error)
 }
