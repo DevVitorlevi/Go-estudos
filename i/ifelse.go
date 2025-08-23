15f347be0ee01bfb40b67ec45fe6f95e8005fd7d
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-08-23 14:14:18 -0300

    feat: if

---
+++ i/ifelse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	numero := 10
+
+	if numero >= 10 {
+		fmt.Printf("O %v e maior ou igual a 10", numero)
+	} else {
+		fmt.Printf("O %v e menor que 10", numero)
+	}
+
+}
