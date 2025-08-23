24218dae718887b38b922f0b666bd5d1227e2480
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-08-23 10:08:51 -0300

    embedding

---
+++ Heranca SQN/heranca.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name   string
+	age    int
+	height float64
+}
+
+type student struct {
+	person
+	course string
+	school string
+}
+
+func main() {
+	p1 := person{"Vitor", 17, 1.71}
+	st1 := student{p1, "Desenvolvimento de Sistemas", "EEEP Jaime"}
+
+	fmt.Println(st1)
+}
